4f745ebab5c6b1d2566e601e8d33f2405ada08d4
Ade Indra Pratama <adeprathama@gmail.com>
2024-01-28 11:42:33 +0700

    Add Router Tutorial

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.get("/", (req, res) => {
     });
 });
 
+require("./app/routes/tutorial.routes.js")(app);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
